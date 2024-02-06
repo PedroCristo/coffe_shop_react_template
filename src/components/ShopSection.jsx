67acb335678a0a68b55dd97ea0de25765d257718
Pedro Cristo <pcristo35@gmail.com>
2024-02-06 10:32:58 +0000

    Add logic to display the percentage or not and add css style for percentage and not promotin available

--- src/components/ShopSection.jsx
+++ src/components/ShopSection.jsx
@@ -1,55 +1,63 @@
 import shopProductsData from "../data/shopProducts";
 
 function ShopSection() {
+  return (
+    <div className="bg-shop" id="shop">
+      <div className="shop-links">
+        <div>
+          <h2>products</h2>
+        </div>
+        <div className="links">
+          <li data-filter="all">all</li>
+          <li data-filter=".coffee">coffee</li>
+          <li data-filter=".machines">machines</li>
+          <li data-filter=".sweets">sweets</li>
+        </div>
+      </div>
+      <div className="shop-flex consts">
+        {shopProductsData.map((product) => {
+          // Calculate discount percentage
+          const discountPercentage =
+            ((product.oldPrice - product.price) / product.oldPrice) * 100;
 
- return (
-   <div className="bg-shop" id="shop">
-     <div className="shop-links">
-       <div>
-         <h2>products</h2>
-       </div>
-       <div className="links">
-         <li data-filter="all">all</li>
-         <li data-filter=".coffee">coffee</li>
-         <li data-filter=".machines">machines</li>
-         <li data-filter=".sweets">sweets</li>
-       </div>
-     </div>
-     <div className="shop-flex consts">
-       {shopProductsData.map((product) => {
-         // Calculate discount percentage
-         const discountPercentage = ((product.oldPrice - product.price) / product.oldPrice) * 100;
-
-         return (
-           <div className="shop1 mix coffee" key={product.id}>
-             <div className="shop-image">
-               <img src={product.imageUrl} alt={product.name} />
-               {/* Display discount percentage */}
-               <div className="price">-{discountPercentage.toFixed(0)}%</div>
-               <div className="social">
-                 {product.social.map((icon) => (
-                   <a key={icon.id} href={icon.linkURL}>
-                     <i className={icon.socialIcon}></i>
-                   </a>
-                 ))}
-               </div>
-             </div>
-             <h2>hot coffee</h2>
-             <div className="stars">
-               {product.stars.map((icon) => (
-                 <i key={icon.id} className={icon.star}></i>
-               ))}
-             </div>
-             {/* Display product price */}
-             <article>€{product.price}</article>
-             {/* Display old price */}
-             <span>€{product.oldPrice}</span>
-           </div>
-         );
-       })}
-     </div>
-   </div>
- );
+          return (
+            <div className="shop1 mix coffee" key={product.id}>
+              <div className="shop-image">
+                <img src={product.imageUrl} alt={product.name} />
+                {/* Display discount percentage */}
+                {discountPercentage === 0 ? (
+                  <div className="price red">Promotion not availabe</div>
+                ) : (
+                  <div className="price green">-{discountPercentage.toFixed(0)}%</div>
+                )}
+                <div className="social">
+                  {product.social.map((icon) => (
+                    <a key={icon.id} href={icon.linkURL}>
+                      <i className={icon.socialIcon}></i>
+                    </a>
+                  ))}
+                </div>
+              </div>
+              <h2>{product.name}</h2>
+              <div className="stars">
+                {product.stars.map((icon) => (
+                  <i key={icon.id} className={icon.star}></i>
+                ))}
+              </div>
+              {/* Display product price */}
+              <article>€{product.price.toFixed(2)}</article>
+              {/* Display old price */}
+              {product.price === product.oldPrice ? (
+                <span></span>
+              ) : (
+                <span>€{product.oldPrice.toFixed(2)}</span>
+              )}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default ShopSection;
