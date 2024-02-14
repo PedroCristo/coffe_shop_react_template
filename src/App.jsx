fc178fb17450ffa28f09e0d8b98ed0355dc9156f
Pedro Cristo <pcristo35@gmail.com>
2024-02-14 10:37:11 +0000

    Add _redirect file to suport client side routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-// import { Route, Routes } from 'react-router-dom'; 
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import HomePage from "./pages/HomePage";
 import ShopPage from "./pages/ShopPage";
@@ -17,8 +16,8 @@ function App() {
       <ScrollTop />
         <Routes>
           <Route path="/" exact element={<HomePage />} />
-          <Route path="shop-page" element={<ShopPage />} />
-          <Route path="menu-page" element={<MenuPage />} />
+          <Route path="/shop-page" element={<ShopPage />} />
+          <Route path="/menu-page" element={<MenuPage />} />
         </Routes>
       <Footer />
     </div>
